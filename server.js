0a79cd4b75b380d431887b639dee0c817bf1f2ac
Aayush <aayush2615@gmail.com>
2026-03-19 00:52:42 +0530

    fix: somme changes in html and css files

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ const upload = multer({
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: process.env.EMAIL_PORT || 587,
+  port: parseInt(process.env.EMAIL_PORT) || 587,
   secure: process.env.EMAIL_SECURE === 'true',
   auth: {
     user: process.env.EMAIL_USER,
